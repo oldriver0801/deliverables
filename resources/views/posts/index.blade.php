e3a1c38638fbfa05c7fcbf206b8d2cc3b4b3d916
oldriver0801 <ec2-user@ip-172-31-17-82.ec2.internal>
2023-01-04 17:06:36 +0900

    マイグレーションファイル作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+    </head>
+    <body>
+        
+    </body>
+</html>
