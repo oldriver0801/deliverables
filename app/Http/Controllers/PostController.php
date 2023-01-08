a64e94ef0cb6dec7fc556dbb483dcc3e7bc491c2
oldriver0801 <ec2-user@ip-172-31-17-82.ec2.internal>
2023-01-08 20:43:49 +0900

    migrationファイル(sneaker)に追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,10 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts'=>$post->get()]);
+    }
 }
 
 ?>
